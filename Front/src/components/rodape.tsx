baddef245671b66336785c94bd9e0ba92692ccac
Bruno Correa <productbdc@gmail.com>
2026-09-03 19:08:21 -0300

    Ajuste rodape front

--- Front/src/components/rodape.tsx
+++ Front/src/components/rodape.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Rodape() {
     return (
-    <footer className="bg-[#1d0014] text-white py-4">
+    <footer className="bg-[#1d0014] text-white py-4" max-width="100%">
         <div className="container mx-auto text-center">
             <p>&copy; 2023 bidbits. Todos os direitos reservados.</p>
         </div>
