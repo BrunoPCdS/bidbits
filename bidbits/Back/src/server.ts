98f1cf2a61b0c60770d7722b0d8cc7f28f21e57a
Bruno Correa <productbdc@gmail.com>
2026-09-17 20:22:24 -0300

    test cors

--- bidbits/Back/src/server.ts
+++ bidbits/Back/src/server.ts
@@ -12,15 +12,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://localhost:5174',
-    'http://127.0.0.1:5174',
-  ],
-  credentials: true,
-}))
+app.use(cors())
 
 app.use("/marcas", routesMarcas)
 app.use("/consoles", routesConsoles)
